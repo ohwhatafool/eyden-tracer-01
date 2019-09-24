de0a8bb13f330976bd6b4054256427c88b0284fa
Drish10 <drishti.serenity@gmail.com>
2019-09-24 17:53:01 +0200

    Figured out Configuration

--- src/ray.h
+++ src/ray.h
@@ -7,7 +7,7 @@
 /// Basic ray structure
 struct Ray
 {
-	Vec3f	org;		///< Origin
-	Vec3f	dir;		///< Direction
-	float	t;			///< Current/maximum hit distance
+	Vec3f org; ///< Origin
+	Vec3f dir; ///< Direction
+	float t;   ///< Current/maximum hit distance
 };
